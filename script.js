a844dadb988b6703d6e90ca2c77e5263ecbfc871
Ксения Чалбай <raketa.souz@gmail.com>
2020-03-15 23:56:03 +0300

    new changes in the files

---
+++ script.js
@@ -0,0 +1,40 @@
+let url = window.location.toString();
+
+let getUserName = (url) => {
+    let arrayUrl = url.split('=');
+    let userName = arrayUrl[1];
+    if (userName  == undefined) {
+        userName = 'Costyanica';
+    }
+    return userName;
+}
+
+let name = getUserName(url);
+
+fetch(`https://api.github.com/users/${name}`)
+  .then(response => response.json())
+  .then(json => {
+            let getImage = () => {
+            let photo = document.querySelector('.photo');
+            photo.src = json.avatar_url;
+            }
+            
+            let getName = () => {
+            let user = document.querySelector('.name');
+            let link = document.querySelector('.link');
+            let theName = json.name;
+            user.innerHTML = theName;
+            link.href = json.html_url;
+            }
+            
+            let getInfo = () => {
+            let info = document.querySelector('.info');
+            info.innerHTML = json.bio;
+            }
+          
+            getImage();
+            getName();
+            getInfo();
+  
+            })
+    .catch(err => alert(err + 'Информация о пользователе не доступна'));
